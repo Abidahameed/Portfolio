66f894214bc66a754132346cd9e49ae3613c1bab
Abida <abida@nomdindia.com>
2025-05-28 16:30:47 +0530

    latest changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,17 +12,12 @@ import { trigger, transition, style, animate, query, group } from '@angular/anim
 })
 export class AppComponent {
   title = 'AbidaPortfolio';
-routes = ['/profile', '/about', '/project', '/contact'];
-  currentIndex = 0;
 
-  constructor(private router: Router) {   }
-  ngOnInit() {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        const url = event.urlAfterRedirects;
-        this.currentIndex = this.routes.indexOf(url);
-      }
-    });
+scrollTo(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
   }
 
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,11 @@
 import { Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { ProjectComponent } from './project/project.component';
+
 
 export const routes: Routes = [
   
   { path: 'profile', 
     component: ProfileComponent ,
 },
-    {
-        path: 'about',
-        component:AboutComponent,
-
-    },
-    {
-        path: 'contact',
-        component:ContactComponent,
-   
-    },
-    {
-        path: 'project',
-        component:ProjectComponent,
-
-        
-    },
     { path: '', redirectTo: '/profile', pathMatch: 'full' },
 ];
